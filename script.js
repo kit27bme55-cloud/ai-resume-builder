358fcda3e446c98c166ec864c0e6722f9501416a
kit27bme55-cloud <kit27.bme55@gmail.com>
2026-05-25 15:56:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+async function generateSummary() {
+
+const skills =
+document.getElementById("skills").value;
+
+const API_KEY = "AIzaSyBOFUksbL-QF17UrdHEsdX5FjbU31IsZtY";
+
+try {
+
+const response = await fetch(
+`https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
+{
+method: "POST",
+
+headers: {
+"Content-Type": "application/json",
+},
+
+body: JSON.stringify({
+contents: [{
+parts: [{
+text:
+`Generate professional resume summary for ${skills}`
+}]
+}]
+})
+});
+
+const data = await response.json();
+
+console.log(data);
+
+if(data.candidates){
+
+document.getElementById("output").innerText =
+data.candidates[0].content.parts[0].text;
+
+}
+else{
+
+document.getElementById("output").innerText =
+"API Error";
+
+console.log(data);
+
+}
+
+}
+catch(error){
+
+console.log(error);
+
+document.getElementById("output").innerText =
+"Something went wrong";
+
+}
+}
